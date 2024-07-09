efb9aeac5cc7f449576f0210a8ffea672d82de93
haikt03 <tuanhai1652003@gmail.com>
2024-07-09 08:09:36 +0700

    Add MobX

--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -1,10 +1,9 @@
 import { Button, Container, Menu } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
-interface Props {
-    openForm: () => void;
-}
+export default function Navbar() {
+    const { activityStore } = useStore();
 
-export default function Navbar({ openForm }: Props) {
     return (
         <Menu inverted fixed="top">
             <Container>
@@ -19,7 +18,7 @@ export default function Navbar({ openForm }: Props) {
                 <Menu.Item name="Activities" />
                 <Menu.Item>
                     <Button
-                        onClick={openForm}
+                        onClick={() => activityStore.openForm()}
                         positive
                         content="Create Activity"
                     />
